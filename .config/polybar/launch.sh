ca948d20ab77611effd3615bfd7df2da8fec908c
Dusan Ilic <dusan@tannera.io>
2023-02-06 16:02:27 +0100

    Added home config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -38,6 +38,20 @@ case $profile in
                 ;;
         esac
         ;;
+    home)
+        case $host in
+            xps)
+                echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+                MONITOR=DP-3 polybar primary 2>&1 | tee -a /tmp/polybar1.log & disown
+                MONITOR=eDP-1 polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+                ;;
+            *)
+                echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+                MONITOR=HDMI2 polybar primary 2>&1 | tee -a /tmp/polybar1.log & disown
+                MONITOR=eDP1 polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+                ;;
+        esac
+        ;;
     *)
         case $host in
             xps)
